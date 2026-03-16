6195e8705aff301ae177df64513f3730526a916d
davidhormes2-debug <251061093+davidhormes2-debug@users.noreply.github.com>
2026-03-16 00:49:59 +0000

    Create sw.js

---
+++ public/public/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'hiring-hub-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
